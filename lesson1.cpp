666a655c63df38e6f14cf770657e08ff91dd3ad6
Ubuntu <hejunqi@ubuntu.(none)>
2018-02-07 07:03:41 -0800

    new file

---
+++ lesson1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+	wchar_t bob = L'p';
+	bool ans = true;
+	bool start = -110;
+	cout << ans << endl << start << endl;
+	wcout << L"tall" << endl;
+	return 0;
+}
